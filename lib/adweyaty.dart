5b047cf7091c9189f2863c86e467eb08bc01d93f
Amr Reda <amr.reda2014@yahoo.com>
2025-10-23 10:03:17 +0300

    initialize the project

---
+++ lib/adweyaty.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Adweyaty extends StatelessWidget {
+  const Adweyaty({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'adweyaty.dart';
+
+void main (){
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(Adweyaty());
+}
