3bdecc272eba6cad1609714a7bd923e9e65ac53c
Amr Reda <amr.reda2014@yahoo.com>
2025-12-04 19:43:29 +0200

    adjust home cubit and state

--- lib/features/home/data/cubit/home_cubit.dart
+++ lib/features/home/data/cubit/home_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:adweyaty_application/features/auth/data/models/user_model.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -5,4 +6,7 @@ part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
+  void loadUserData({required UserModel userDataLoaded}){
+    emit(HomeDataSuccess(userDataLoaded));
+  }
 }
--- lib/features/home/data/cubit/home_state.dart
+++ lib/features/home/data/cubit/home_state.dart
@@ -4,3 +4,13 @@ part of 'home_cubit.dart';
 sealed class HomeState {}
 
 final class HomeInitial extends HomeState {}
+
+final class HomeDataLoading extends HomeState {}
+final class HomeDataSuccess extends HomeState {
+   final UserModel user ;
+   HomeDataSuccess(this.user);
+}
+final class HomeDataError extends HomeState {
+  final String message ;
+  HomeDataError(this.message);
+}
