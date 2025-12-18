1647721a9b2422ccb9ea9a9fa9a252119e3be543
Amr Reda <amr.reda2014@yahoo.com>
2025-12-18 03:45:52 +0200

    edit Home categry model and make home repo

---
+++ lib/features/home/data/home_repo/home_repo.dart
@@ -0,0 +1,22 @@
+import 'package:adweyaty_application/features/home/data/model/homeCategoryModel.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class HomeRepo {
+  final firestore = FirebaseFirestore.instance ;
+
+  Future<List<HomeCategoryModel>> getCategories () async{
+    try{
+      final snapshot = await firestore.collection('categories').get();
+
+      return snapshot.docs
+          .map(
+            (doc) => HomeCategoryModel.fromJson(doc.data(), doc.id),
+      ).toList();
+    }catch(e){
+      throw Exception('Failed to load categories') ;
+    }
+
+
+  }
+
+}
--- lib/features/home/data/model/homeCategoryModel.dart
+++ lib/features/home/data/model/homeCategoryModel.dart
@@ -8,6 +8,14 @@ class HomeCategoryModel {
 
   const HomeCategoryModel({required this.id, required this.image ,required this.title});
 
+
+   factory HomeCategoryModel.fromJson ( Map<String,dynamic>json,String docId) {
+     return HomeCategoryModel(
+         id: docId,
+         image: json["image"],
+         title: json["title"]
+     );
+   }
 }
 
    const List<HomeCategoryModel> homeCategoryModel =
@@ -15,6 +23,7 @@ class HomeCategoryModel {
     HomeCategoryModel(id:"0",image: AppImages.coughMedicines, title: "cough medicines"),
     HomeCategoryModel(id:"1",image: AppImages.headache, title: "headache medicines"),
     HomeCategoryModel(id:"2",image: AppImages.temperature, title: "temperature medicines"),
-    HomeCategoryModel(id:"3",image: AppImages.womanProduct, title: "woman medicines"),
+    HomeCategoryModel(id:"3",image: AppImages.womanProduct, title: "woman products"),
     HomeCategoryModel(id:"4",image: AppImages.vitamin, title: "vitamin medicines"),
+    HomeCategoryModel(id:"4",image: AppImages.bloodPressure, title: "pressure medicines"),
 ];
