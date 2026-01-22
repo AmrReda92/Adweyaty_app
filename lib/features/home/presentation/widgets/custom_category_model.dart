e8b0f296a830613c93e0301a80d8b3996b63caf5
Amr Reda <amr.reda2014@yahoo.com>
2026-01-22 23:13:39 +0200

    added intl files again

--- lib/features/home/presentation/widgets/custom_category_model.dart
+++ lib/features/home/presentation/widgets/custom_category_model.dart
@@ -3,6 +3,7 @@ import 'package:adweyaty_application/core/theme/app_text_style.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../generated/l10n.dart';
 import '../../data/model/homeCategoryModel.dart';
 
 class CustomCategoryModel extends StatelessWidget {
